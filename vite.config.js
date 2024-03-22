2d31bf459ab54c2bc04e33ee5b672d4079d7fdd1
Mariah Marsolek <mamarsolek@gmail.com>
2024-03-21 22:30:09 -0700

    Remove dependancies and update vite config

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,8 @@
 import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
-import { viteSingleFile } from "vite-plugin-singlefile"
 
 
 
 export default defineConfig({
-	plugins: [sveltekit(), viteSingleFile({ useRecommendedBuildConfig: false})]
+	plugins: [sveltekit()]
 });
